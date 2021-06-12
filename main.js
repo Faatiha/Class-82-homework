fa78b842068a5672c42cf914c9761caa9dfb02dd
Faatiha <66500499+Faatiha@users.noreply.github.com>
2021-06-12 10:34:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,59 +1,63 @@
-var mouseEvent = "empty";
 var last_position_of_x, last_position_of_y;
 
-canvas = document.getElementById('myCanvas');
+canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
+
 color = "black";
-width_of_line = 1;
+radius = 20;
+width_of_line = 10;
+mouseEvent = "empty";
 
 canvas.addEventListener("mousedown", my_mousedown);
 
 function my_mousedown(e)
 {
-color = document.getElementById("color").value;
-width_of_line = document.getElementById("width_of_line").value;
-mouseEvent = "mouseDown";
+    color = document.getElementById("color").value;
+    radius = document.getElementById("radius").value;
+    width_of_line = document.getElementById("width_of_line").value;
+    mouseEvent = "mousedown";
+}
+
+canvas.addEventListener("mouseup", my_mouseup);
+
+function my_mouseup(e)
+{
+    color = document.getElementById("color").value;
+    radius = document.getElementById("radius").value;
+    width_of_line = document.getElementById("width_of_line").value;
+    mouseEvent = "mouseup";
+}
+
+canvas.addEventListener("mouseleave", my_mouseleave);
+
+function my_mouseleave(e)
+{
+    color = document.getElementById("color").value;
+    radius = document.getElementById("radius").value;
+    width_of_line = document.getElementById("width_of_line").value;
+    mouseEvent = "mouseleave";
 }
 
 canvas.addEventListener("mousemove", my_mousemove);
 
 function my_mousemove(e)
 {
-current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-
-if (mouseEvent == "mouseDown")
-{
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width_of_line;
-    console.log("Last Position of x and y coordinates = ");
-    console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-    ctx.moveTo(last_position_of_x, last_position_of_y);
-
-    console.log("Current Position of x and y coordinates = ");
-    console.log("x = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-    ctx.moveTo(current_position_of_mouse_x, current_position_of_mouse_y); 
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
     
-    ctx.stroke();
-}
-last_position_of_x = current_position_of_mouse_x;
-last_position_of_y = current_position_of_mouse_y;
-}
-
-canvas.addEventListener("mouseup", my_mouseup);
-function my_mouseup(e)
-{
-    mouseEvent = "mouseUp";
-}
-
-canvas.addEventListener("mouseleave", my_mouseleave);
-function my_mouseleave(e)
-{
-    mouseEvent = "mouseLeave";
+ if (mouseEvent == "mousedown")
+    {
+        console.log("Current position of x and y coordinates = ");
+        console.log("x = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        ctx.beginPath();
+        ctx.strokestyle = color;
+        ctx.lineWidth = width_of_line;
+        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
 }
 
-function clearArea()
-{
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-}
+
+
+
+    
